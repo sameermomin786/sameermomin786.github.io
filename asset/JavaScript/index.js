6b55c2235647f3c1740574a89176c105e3d4af96
sameermomin786 <mominsameer304@gmail.com>
2022-09-04 16:03:12 +0530

    my profile

---
+++ asset/JavaScript/index.js
@@ -0,0 +1,19 @@
+// console.log('hello sam');
+
+// $(".animated-progress span ").each(function (){
+//     $(this).animate(
+//         {
+//             width: $(this).attr("data-progress") + "%" 
+
+//         },
+//         1000
+//     );
+//     $(this).text($(this).attr("data-progress") + "%")
+// });
+
+
+// let arr = ['3',['name','sameer', 'name','abuzar']];
+
+let arr1 = arr[1][1];
+let arr2  = arr[2][2];
+console.log(arr1);
