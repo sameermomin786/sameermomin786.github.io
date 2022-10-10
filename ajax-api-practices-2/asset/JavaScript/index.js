9288e08d33ab9875ad79ff4187c87543984d524f
sameermomin786 <mominsameer304@gmail.com>
2022-10-11 00:07:16 +0530

    ajax-api

---
+++ ajax-api-practices-2/asset/JavaScript/index.js
@@ -0,0 +1,73 @@
+
+$.ajax({
+	type: "GET",
+	url: `
+    http://103.88.221.150:9439/api/staff/getAll
+    `,
+	responseType: "json",
+	success: function (response) {
+        console.log("check-staf=>api",response);
+        $.each(response, function (index, value) { 
+            console.log("check-each=>",value);
+            $("#staf-details").append(`
+            <tr>
+            <td>${value.staffId}</td>
+            <td>${value.staffName}</td>
+            <td>${value.email}</td>
+            <td>${value.contactNumber}</td>
+            <td>${value.address}</td>
+            
+            </tr>
+            
+            `);
+             
+        });
+       
+	},
+    complete: function(){
+        $("#table-details").DataTable();
+    }
+	
+});
+
+
+$("#save-details").on('click', function () {
+    console.log("hello");
+
+    let staffName = $("#staff-Name").val()
+    console.log("staffName",staffName);
+
+    let StaffPhoneNumber = $("#phone-number").val()
+    console.log("StaffPhoneNumber",StaffPhoneNumber);
+    
+        let staffEmail = $("#email").val()
+        console.log("staffEmail",staffEmail);
+
+    let StaffAddress = $("#Address").val()
+    console.log("StaffAddress",StaffAddress);
+
+    
+$.ajax({
+    url: `http://103.88.221.150:9439/api/staff`,
+    dataType: 'json',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({
+       
+            "address": StaffAddress,
+            "contactNumber": StaffPhoneNumber,
+            "createdBy": "Admin tast",
+            "createdId": 1,
+            "email":staffEmail, 
+            "staffName": staffName
+        
+    }),
+    success: function (response) {
+      
+    },
+    
+
+});
+});
+
+
--- ajax-api-practices/asset/JavaScritp/index.js
+++ ajax-api-practices/asset/JavaScritp/index.js
@@ -30,5 +30,38 @@ $("#tabledetail").DataTable();
 })
 
 function getBankdata(){
-    
 }
+
+
+$("#save-bank").on('click', function () {
+
+let bankName = $("#bank-Name").val()
+console.log("bankname",bankName); 
+
+let accountNumber = $("#account-number").val()
+console.log("bankname",accountNumber); 
+
+let ifscCode = $("#ifcs-code").val()
+console.log("bankname",ifscCode); 
+
+$.ajax({
+    url: `http://103.88.221.150:9439/api/bank`,
+    dataType: 'json',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({
+       
+            "accountNumber": accountNumber,
+            "bankName": bankName,
+            "createdBy": "Admin tast",
+            "createdId": 1,
+            "ifscCode":ifscCode, 
+        
+    }),
+    success: function (response) {
+      
+    },
+    
+
+});
+});
